0e605fa84f478654fda2451c5920c20f2a8873a9
lazy <ddlazy@qq.com>
2025-10-18 15:27:04 +0800

    调整vxetable

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@
 import { pinia } from "@/pinia"
 import { router } from "@/router"
 import { installPlugins } from "@/plugins"
+import VxePcUi from "vxe-pc-ui"
 import App from "@/App.vue"
 // css
 import "normalize.css"
 import "nprogress/nprogress.css"
-import "element-plus/theme-chalk/dark/css-vars.css"
 import "vxe-table/lib/style.css"
+import "vxe-pc-ui/lib/style.css"
+import "ant-design-vue/dist/reset.css"
 import "@@/assets/styles/index.scss"
 
 // 创建应用实例
@@ -19,7 +21,7 @@ const app = createApp(App)
 installPlugins(app)
 
 // 安装 pinia 和 router
-app.use(pinia).use(router)
+app.use(pinia).use(router).use(VxePcUi)
 
 // router 准备就绪后挂载应用
 router.isReady().then(() => {
